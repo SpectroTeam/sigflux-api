a21127fa1ecac47ed4fe517cc4c813bce103b843
luizfernandoin <luizfernandoinfo100@gmail.com>
2026-01-17 11:42:07 -0300

    feat: register patient routes in main router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import healthRoutes from './health';
 import authRoutes from './auth';
+import patientRoutes from './patients';
 
 const router = Router();
 
 router.use('/health', healthRoutes);
 router.use('/auth', authRoutes);
+router.use('/patients', patientRoutes);
 
 export default router;
